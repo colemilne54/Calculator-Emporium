b6781051116e021267efccc9140fe0e09a077893
colemilne54 <colemilne54@gmail.com>
2023-03-27 13:07:29 -0600

    1 file modified

--- src/Components/Calculators/WindChill.jsx
+++ src/Components/Calculators/WindChill.jsx
@@ -7,7 +7,69 @@ import {
   Typography,
   FormGroup,
   Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper
 } from '@mui/material';
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+var tempArr = [];
+
+for(var i=40; i>=-45; i-=5) {
+  tempArr.push(i);
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
+
+function FrostBiteChart() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="frostbite table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Calm</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 export default function WindChill() {
   const [temp, setTemp] = useState('');
@@ -77,6 +139,8 @@ export default function WindChill() {
       <Typography variant="p" component="p">
         {(temp != '' && wind != '') && `${calculateWindChill()}`}
       </Typography>
+      <FrostBiteChart />
     </>
   );
 }
+
