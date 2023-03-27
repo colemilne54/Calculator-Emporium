8947da0899ce3912fcb2a32a3755484dbbb9059b
colemilne54 <colemilne54@gmail.com>
2023-03-27 15:23:34 -0600

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import RandomNumberGenerator from './Components/Calculators/RandomNumberGenerator';
+import AppAppBar from './Components/UI/AppBar';
 
 export default function App() {
   return (
-    <div>
+    <>
+      <AppAppBar />
       <RandomNumberGenerator />
-    </div>
+    </>
   );
 }
