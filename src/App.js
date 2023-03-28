73b6af50a047e31a8301c8c0124a36cda1e24e3a
colemilne54 <colemilne54@gmail.com>
2023-03-27 23:15:52 -0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './style.css';
 import RandomNumberGenerator from './Components/Calculators/RandomNumberGenerator';
 import AppAppBar from './Components/UI/AppBar';
 import AppFooter from './Components/UI/AppFooter';
+import DogYears from './Components/Calculators/DogYears'
 
 export default function App() {
   return (
     <>
       <AppAppBar />
       <RandomNumberGenerator />
+      {/* <DogYears /> */}
       {/* <AppFooter /> */}
     </>
   );
