cef9c8bd9a719625b18ad1d979267e13d09e4e81
Cole Milne <colemilne54@gmail.com>
2023-04-16 16:24:14 -0600

    sidbar done. time for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import './style.css';
+import './index.css';
 import RandomNumberGenerator from './Components/Calculators/RandomNumberGenerator';
 import AppAppBar from './Components/UI/AppBar';
 import AppFooter from './Components/UI/AppFooter';
 import DogYears from './Components/Calculators/DogYears';
 import BarbellWeight from './Components/Calculators/BarbellWeight';
 import DaysUntil from './Components/Calculators/DaysUntil';
+import SideBar from './Components/UI/SideBar';
 
 export default function App() {
   return (
     <>
       <div className="flex">
-        <DaysUntil />
+        <SideBar />
+        {/* <DaysUntil /> */}
+        <DogYears />
       </div>
     </>
   );
--- src/Components/UI/SideBar.js
+++ src/Components/UI/SideBar.js
@@ -1,13 +1,28 @@
+import { FaDog, FaRuler } from 'react-icons/fa';
+import { GiWeightLiftingUp, GiWeightScale, GiCardRandom } from 'react-icons/gi'
+import { RiWindyFill } from "react-icons/ri";
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
 const SideBar = () => {
     return (
-        <div className="fixed">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+        <div className="fixed top-0 left-0 h-screen w-16 m-0
+                        flex flex-col 
+                        bg-primary text-secondary shadow-lg">
+
+            <SideBarIcon icon={<FaDog size="28" />} text="Dog Years Calculator" />
         </div>
     );
 };
+
+const SideBarIcon = ({ icon, text }) => (
+    <div className="sidebar-icon group">
+        {icon}
+
+        <span class="sidebar-tooltip group-hover:scale-100">
+            {text}
+        </span>
+    </div>
+)
 
 export default SideBar;
