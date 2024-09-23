07a8cc55166af43dd9a792bccb04704492257c5e
Cole Milne <colemilne54@gmail.com>
2024-09-22 17:16:20 -0700

    appbar

--- src/components/ui/AppBar.js
+++ src/components/ui/AppBar.js
@@ -5,50 +5,55 @@ import AppBar from './components/AppBar';
 import Toolbar from './components/Toolbar';
 
 const rightLink = {
-  fontSize: 16,
-  color: 'common.white',
-  ml: 3,
+   fontSize: 16,
+   color: 'common.white',
+   ml: 3,
 };
 
 function AppAppBar() {
-  return (
-    <div id="header-div" className="bg-blue-500">
-      <AppBar position="fixed" className="bg-blue-500">
-        <Toolbar className="bg-blue-500" sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{ flex: 1 }} className="bg-blue-500"/>
-          <Link
-            variant="h6"
-            underline="none"
-            color="inherit"
-            href="/"
-            sx={{ fontSize: 24 }}
-          >
-            {'Calculator Emporium'}
-          </Link>
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
-            <Link
-              color="inherit"
-              variant="h6"
-              underline="none"
-              href="/"
-              sx={rightLink}
+   return (
+      <div id="header-div" className="bg-blue-500">
+         <AppBar position="fixed" className="bg-blue-500">
+            <Toolbar
+               className="bg-blue-500"
+               sx={{ justifyContent: 'space-between' }}
             >
-              {/* {'Sign In'} */}
-            </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              href="/"
-              sx={{ ...rightLink, color: 'secondary.main' }}
-            >
-              {/* {'Sign Up'} */}
-            </Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </div>
-  );
+               <Box sx={{ flex: 1 }} className="bg-blue-500" />
+               <Link
+                  variant="h6"
+                  underline="none"
+                  color="inherit"
+                  href="/"
+                  sx={{ fontSize: 24 }}
+               >
+                  {'Calculator Emporium'}
+               </Link>
+               <Box
+                  sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}
+               >
+                  <Link
+                     color="inherit"
+                     variant="h6"
+                     underline="none"
+                     href="/"
+                     sx={rightLink}
+                  >
+                     {/* {'Sign In'} */}
+                  </Link>
+                  <Link
+                     variant="h6"
+                     underline="none"
+                     href="/"
+                     sx={{ ...rightLink, color: 'secondary.main' }}
+                  >
+                     {/* {'Sign Up'} */}
+                  </Link>
+               </Box>
+            </Toolbar>
+         </AppBar>
+         <Toolbar />
+      </div>
+   );
 }
 
 export default AppAppBar;
