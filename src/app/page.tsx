86c55ba4ea6b08f4eb05b64d62ded8e2e3253ea1
Cole Milne <colemilne54@gmail.com>
2024-09-22 17:45:55 -0700

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <h1>home</h1>
-  );
+   return (
+      <div className="flex">
+         <p>
+            Choose on of the Calculators above to begin exploring the Emporium
+         </p>
+      </div>
+   );
 }
